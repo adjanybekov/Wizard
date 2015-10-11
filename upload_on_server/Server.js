f9131aa13364ce4c5801c9dd092f906aea06cbfe
Azat <adjanybekov@mail.ru>
2015-10-11 21:39:43 +0300

    upload Already made

---
+++ upload_on_server/Server.js
@@ -0,0 +1,33 @@
+var express         =       require("express");
+var multer          =       require('multer');
+var app             =       express();
+var upload      =   multer({ dest: './uploads/'});
+
+app.use(multer({ dest: './uploads/',
+    rename: function (fieldname, filename) {
+        return filename+Date.now();
+    },
+    onFileUploadStart: function (file) {
+        console.log(file.originalname + ' is starting ...');
+    },
+    onFileUploadComplete: function (file) {
+        console.log(file.fieldname + ' uploaded to  ' + file.path)
+    }
+}));
+
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
+});
+
+app.listen(3000,function(){
+    console.log("Working on port 3000");
+});
